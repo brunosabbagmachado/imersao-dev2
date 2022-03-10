2d8f13706a6946f84a6fc20c1f9b7f60d0376caf
Bruno Machado <bruno.sabbagmachado@outlook.com>
2022-03-10 11:53:18 -0300

    projetos moedas, anos-luz, temperatura atualizados

--- js/script.js
+++ js/script.js
@@ -1,31 +1,63 @@
-function ConverterEmReal() {
+function ConverterDolar() {
+  var valorElemento = document.getElementById("valorMoedas");
+  var valor = valorElemento.value;
+  var valorEmDolarNumerico = parseFloat(valor);
 
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseFloat(valor);
+  console.log("Valor digitado: " + valorEmDolarNumerico);
 
-    //console.log(valorEmDolarNumerico);
+  var valorEmReal = valorEmDolarNumerico * 5;
+  var valorEmEuro = valorEmDolarNumerico * 0.91;
+  var valorEmIene = valorEmDolarNumerico * 116.1;
+  var valorEmPesoArgentino = valorEmDolarNumerico * 108.77;
 
-    var valorEmRealNumerico = valorEmDolarNumerico * 5;
-    //console.log(valorEmRealNumerico);
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertidoEmReal = "O resultado em real é R$ " + valorEmReal;
+  var valorConvertidoEmEuro = "O resultado em euro é € " + valorEmEuro;
+  var valorConvertidoEmIene = "O resultado em iene é ¥ " + valorEmIene;
+  var valorConvertidoEmPesoArgentino = "O resultado em peso argentino é $ " + valorEmPesoArgentino;
 
-    var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em real é R$ " + valorEmRealNumerico;
-    elementoValorConvertido.innerHTML = valorConvertido;
+  elementoValorConvertido.innerHTML =
+    valorConvertidoEmReal +
+    "<br>" +
+    valorConvertidoEmEuro +
+    "<br>" +
+    valorConvertidoEmIene +
+    "<br>" +
+    valorConvertidoEmPesoArgentino;
 }
 
-function ConverterEmEuro() {
-
-    var valorElemento = document.getElementById("valor");
+function ConverterAnosLuz() {
+    var valorElemento = document.getElementById("valorAnosLuz");
     var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseFloat(valor);
-
-    //console.log(valorEmDolarNumerico);
-
-    var valorEmRealNumerico = valorEmDolarNumerico * 0.91;
-    //console.log(valorEmRealNumerico);
+    var valorEmQuilometros = parseFloat(valor);
+  
+    console.log("Valor digitado: " + valorEmQuilometros);
+  
+    var valorEmAnosLuz = valorEmQuilometros * 0.00000000000010570;
 
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em euro é € " + valorEmRealNumerico;
-    elementoValorConvertido.innerHTML = valorConvertido;
-}
+    var valorConvertidoEmAnosLuz = "O resultado em anos-luz é " + valorEmAnosLuz;
+  
+    elementoValorConvertido.innerHTML = valorConvertidoEmAnosLuz;
+  }
+
+  function ConverterTemperatura() {
+    var valorElemento = document.getElementById("valorTemperatura");
+    var valor = valorElemento.value;
+    var valorEmCelsius = parseFloat(valor);
+  
+    console.log("Valor digitado: " + valorEmCelsius);
+  
+    var valorEmKelvin = valorEmCelsius * 273.15;
+    var valorEmFahrenheit = (valorEmCelsius * 9/5) + 32;
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertidoEmKelvin = "O resultado em Kelvin é " + valorEmKelvin + " K";
+    var valorConvertidoEmFahrenheit = "O resultado em Fahrenheit é " + valorEmFahrenheit + " °F";
+
+
+    elementoValorConvertido.innerHTML = 
+    valorConvertidoEmKelvin +
+    "<br>" +
+    valorConvertidoEmFahrenheit;
+  }
