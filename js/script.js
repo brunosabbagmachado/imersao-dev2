510957b83b76153dfc79f5d83742ce6ea944cd9a
Bruno Machado <bruno.sabbagmachado@outlook.com>
2022-03-10 09:30:14 -0300

    finalizando imersao dev 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function Converter() {
+function ConverterEmReal() {
 
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
@@ -10,6 +10,22 @@ function Converter() {
     //console.log(valorEmRealNumerico);
 
     var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em real é R$" + valorEmRealNumerico;
+    var valorConvertido = "O resultado em real é R$ " + valorEmRealNumerico;
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
+
+function ConverterEmEuro() {
+
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+
+    //console.log(valorEmDolarNumerico);
+
+    var valorEmRealNumerico = valorEmDolarNumerico * 0.91;
+    //console.log(valorEmRealNumerico);
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "O resultado em euro é € " + valorEmRealNumerico;
     elementoValorConvertido.innerHTML = valorConvertido;
 }
